2f72219b72ee8c87f8c1f01b88c886b0f29c38b4
Ronny Dewaele <52859243+polygonwood@users.noreply.github.com>
2023-11-13 22:24:35 +0100

    Delete bpmn/MIK directory

--- bpmn/MIK/loadMIKLocal.sh
+++
@@ -1,5 +0,0 @@
-zbctl deploy 'ZB SOP241v8.bpmn' --insecure --address localhost:26500
-#zbctl deploy 'ZB SOP241 CPV shadows.bpmn' --insecure --address localhost:26500
-#zbctl deploy 'ZB SOP241 decide shadowing.bpmn' --insecure --address localhost:26500
-#zbctl deploy 'ZB SOP241 monitor.bpmn' --insecure --address localhost:26500
-zbctl deploy 'ZB SOP241 Report.bpmn' --insecure --address localhost:26500
